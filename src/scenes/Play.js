32a4b06f6873109d585c85c47efe7ec77c6fcb77
William Gadd <student@ucsc.edu>
2020-04-12 19:16:54 -0700

    blowing up a ship makes a sound

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -46,16 +46,51 @@ class Play extends Phaser.Scene {
             frames: this.anims.generateFrameNumbers('explosion', { start: 0, end: 9, first: 0}),
             frameRate: 30
         });
+
+        // score
+        this.p1Score = 0;
+
+         // score display
+         let scoreConfig = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+        this.scoreLeft = this.add.text(69, 54, this.p1Score, scoreConfig);
+
+        // game over flag
+        this.gameOver = false;
+
+        // 60-second play clock
+        scoreConfig.fixedWidth = 0;
+        this.clock = this.time.delayedCall(60000, () => {
+            this.add.text(game.config.width/2, game.config.height/2, 'GAME OVER', scoreConfig).setOrigin(0.5);
+            this.add.text(game.config.width/2, game.config.height/2 + 64, '(F)ire to Restart', scoreConfig).setOrigin(0.5);
+            this.gameOver = true;
+        }, null, this);
     }
 
     update() {
-        this.starfield.tilePositionX -= 4;
-        this.p1Rocket.update();
+        // check key input for restart
+        if (this.gameOver && Phaser.Input.Keyboard.JustDown(keyF)) {
+            this.scene.restart(this.p1Score);
+        }
 
-        // update spaceships (x3)
-        this.ship01.update();             
-        this.ship02.update();
-        this.ship03.update();
+        this.starfield.tilePositionX -= 4;
+        
+        if (!this.gameOver) {               
+            this.p1Rocket.update();         // update rocket sprite
+            this.ship01.update();           // update spaceships (x3)
+            this.ship02.update();
+            this.ship03.update();
+        } 
 
         // check collisions
         if(this.checkCollision(this.p1Rocket, this.ship03)) {
@@ -71,7 +106,7 @@ class Play extends Phaser.Scene {
             this.shipExplode(this.ship01);
         }
     }
-    
+
     checkCollision(rocket, ship) {
         // simple AABB checking
         if (rocket.x < ship.x + ship.width && 
@@ -94,6 +129,13 @@ class Play extends Phaser.Scene {
             ship.alpha = 1;                     // make ship visible again
             boom.destroy();                     // remove explosion sprite
         });       
+
+        // score increment and repaint
+        this.p1Score += ship.points;
+        this.scoreLeft.text = this.p1Score; 
+        
+        //play sound
+        this.sound.play('sfx_explosion');
     }
 
 
