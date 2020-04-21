682255b65efc66c97201c34e2b0bff1d1b99cae5
William Gadd <student@ucsc.edu>
2020-04-21 13:16:19 -0700

    rocket moves after fired

--- prefabs/Rocket.js
+++ prefabs/Rocket.js
@@ -13,17 +13,15 @@ class Rocket extends Phaser.GameObjects.Sprite {
 
     update() {
         //left/right movement
-        if(!this.isFiring)
+        if(keyLEFT.isDown && this.x >= 47)
         {
-            if(keyLEFT.isDown && this.x >= 47)
-            {
-                this.x -= 2;
-            } 
-            else if (keyRIGHT.isDown && this.x <= 578)
-            {
-                this.x += 2;
-            }
+            this.x -= 2;
+        } 
+        else if (keyRIGHT.isDown && this.x <= 578)
+        {
+            this.x += 2;
         }
+    
 
         //fire button
         if(Phaser.Input.Keyboard.JustDown(keyF) && !this.isFiring)
